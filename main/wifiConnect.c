0d6a19de4f46b1a3665cc1c4049c8e9982114f9b
SudeKun <sudekunduroglu@gmail.com>
2025-09-04 15:16:40 +0300

    fix: Add MAC vendor lookup to run alongside ARP

--- main/wifiConnect.c
+++ main/wifiConnect.c
@@ -25,8 +25,8 @@
 #define TCP_FAILURE 1 << 1
 #define MAX_FAILURES 10
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "WiserAP_24"
+#define WIFI_PASSWORD "Potri8utheB"
 
 /** GLOBALS **/
 
